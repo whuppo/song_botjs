b99219c5eeef69258a715df5adcec305c6165f3a
whuppo <whuppo@whuppo.com>
2022-04-08 11:54:44 +0800

    attempt to classify json data

--- data/activities.ts
+++ data/activities.ts
@@ -1,7 +1,34 @@
-export interface ActivitiesInstance {
+export interface ActivitiesJSON {
     id: string;
     label: string;
     description: string;
     emoji?: string;
-    value: string | ActivitiesInstance[];
+    readonly value: string | ReadonlyArray<ActivitiesJSON>;
+}
+
+export class Activities {
+    id: string;
+    label: string;
+    description: string;
+    emoji?: string;
+    readonly value: string | ReadonlyArray<ActivitiesJSON>;
+
+    isSubgroup() {
+        return Array.isArray(this.value);
+    }
+
+    constructor(model: ActivitiesJSON) {
+        this.id = model.id;
+        this.label = model.description;
+        this.description = model.description;
+        this.emoji = model.emoji;
+        
+        if (Array.isArray(model.value)) {
+            const _constructor = this.constructor as new (...args: any[]) => this;
+            this.value = model.value.map(child => new _constructor(child));
+        }
+        else {
+            this.value = model.value;
+        }
+    }
 }
