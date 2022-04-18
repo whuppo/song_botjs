d75ffa80e033b9691a9cb152385aa165cbf3ed6f
whuppo <whuppo@whuppo.com>
2022-04-18 19:59:33 +0800

    this is where i learned about type guards wow!!

--- commands/lfg/lfg-post.ts
+++ commands/lfg/lfg-post.ts
@@ -1,15 +1,34 @@
-import { Channel, MessageEmbed } from "discord.js"
+import { MessageEmbed } from "discord.js"
+import { client } from "../.."
 import { LFGInstance } from "../../models/lfg"
+import { LFGPost } from "../../models/lfg-post"
+import { ServerSettings } from "../../models/settings"
 
 function createLFGEmbed(data: LFGInstance) {
     const embed = new MessageEmbed()
         .setTitle("test")
 }
 
-export async function createLFGPost(channel: Channel) {
-    
+export async function createLFGPost(data: LFGInstance, server_id: string) {
+    const server_setting = await ServerSettings.findOne({
+        where: { server_id: server_id }
+    });
+
+    if (!server_setting) return;
+
+    const post_channel = client.channels.cache.find(channel => channel.id == server_setting.lfg_channel);
+
+    if (!post_channel) return;
+
+    if (!post_channel.isText()) return;
+
+    post_channel.send("lfg_text");
 }
 
 export async function updateLFGPost(data: LFGInstance) {
+    const posts = await LFGPost.findAll({
+        where: { lfg_id: data.lfg_id }
+    });
+
 
 }
