f1c38dd7ab1ed2d959b6ab74be724956449d2555
whuppo <whuppo@whuppo.com>
2022-04-18 19:26:18 +0800

    server linking/subscribing SQL

---
+++ models/lfg-subscribe.ts
@@ -0,0 +1,19 @@
+import { Model } from "sequelize-typescript";
+import { DataTypes } from "sequelize";
+import { getDB } from "../database/database";
+import { ServerSettings } from "./settings";
+
+const db = getDB();
+
+export interface LFGSubscribeInstance extends Model {
+    server_id: string,
+    subscribed_server_id: string
+}
+
+export const LFGSubscribe = db.define<LFGSubscribeInstance>("LFGSubscribe", {
+    server_id: DataTypes.TEXT,
+    subscribed_server_id: DataTypes.TEXT
+});
+
+LFGSubscribe.belongsTo(ServerSettings, { foreignKey: "server_id" });
+ServerSettings.hasMany(LFGSubscribe);
